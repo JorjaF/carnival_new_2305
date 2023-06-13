212e40e4aa6bf8634cdc6ff42fadebd52ea41f02
Jorja Fleming <aset284@gmail.com>
2023-06-13 11:16:26 -0600

    Add total_revenu and Most_profitable_ride methods?

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,11 +1,13 @@
 class Carnival
   
   attr_reader :duration, 
-              :rides
+              :rides,
+              :revenue_per_ride
 
   def initialize(duration)
     @duration = duration
     @rides = []
+    @revenue_per_ride = {}
   end
 
   def add_ride(ride)
@@ -13,8 +15,20 @@ class Carnival
   end
 
   def most_popular_ride
-    @rides.max_by do |ride|
+      @rides.max_by do |ride|
       ride.rider_log.length
+    end
+  end
+
+  def most_profitable_ride
+    @rides.max_by do |ride|
+      ride.total_revenue
+    end
+  end
+
+  def total_revenue
+    @rides.sum do |ride|
+      ride.total_revenue
     end
   end
 end
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -24,34 +24,96 @@ RSpec.describe Carnival do
     expect(carnival.rides).to eq([ride1, ride2, ride3])
   end
 
-    it "can tell the most popular ride" do
-      carnival = Carnival.new(14)
-      ride1 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 1 })
-      ride2 = Ride.new({ name: 'Bumper Cars', min_height: 48, admission_fee: 3 })
-      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 52, admission_fee: 10 })
+  it "can tell the most popular ride" do
+    carnival = Carnival.new(14)
+    ride1 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 1 })
+    ride2 = Ride.new({ name: 'Bumper Cars', min_height: 48, admission_fee: 3 })
+    ride3 = Ride.new({ name: 'Roller Coaster', min_height: 52, admission_fee: 10 })
+    
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+    
+    visitor1.add_preference(:gentle)
+    visitor1.add_preference(:thrilling)
+    visitor2.add_preference(:gentle)
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:gentle)
+    visitor3.add_preference(:thrilling)
+
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+    carnival.add_ride(ride3)
+
+    ride3.board_rider(visitor3)
+    ride3.board_rider(visitor3)
+    ride2.board_rider(visitor1)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor3)
+
+    expect(carnival.most_popular_ride).to eq(ride1)
+  end
+
+  it "can find the most profitable ride" do
+    carnival = Carnival.new(14)
+    ride1 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 1, excitement: :gentle })
+    ride2 = Ride.new({ name: 'Bumper Cars', min_height: 48, admission_fee: 3, excitement: :gentle})
+    ride3 = Ride.new({ name: 'Roller Coaster', min_height: 52, admission_fee: 7, excitement: :thrilling})
       
-      visitor2 = Visitor.new('Tucker', 36, '$5')
-      visitor1 = Visitor.new('Bruce', 54, '$10')
-      visitor3 = Visitor.new('Penny', 64, '$15')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+  
+    visitor1.add_preference(:gentle)
+    visitor1.add_preference(:thrilling)
+    visitor2.add_preference(:gentle)
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:gentle)
+    visitor3.add_preference(:thrilling)
+  
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+    carnival.add_ride(ride3)
+  
+    ride3.board_rider(visitor3)
+    ride3.board_rider(visitor3)
+    ride2.board_rider(visitor1)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor3)
+  
+    expect(carnival.most_profitable_ride).to eq(ride3)
+  end
 
-      carnival.add_ride(ride1)
-      carnival.add_ride(ride2)
-      carnival.add_ride(ride3)
+  it "can calculate total revenue from all rides" do
+    carnival = Carnival.new(14)
+    ride1 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 1, excitement: :gentle })
+    ride2 = Ride.new({ name: 'Bumper Cars', min_height: 48, admission_fee: 3, excitement: :gentle})
+    ride3 = Ride.new({ name: 'Roller Coaster', min_height: 52, admission_fee: 7, excitement: :thrilling})
+    
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor3 = Visitor.new('Penny', 64, '$15')
 
-      ride3.board_rider(visitor3)
-      ride3.board_rider(visitor3)
-      ride2.board_rider(visitor1)
-      ride1.board_rider(visitor1)
-      ride1.board_rider(visitor2)
-      ride1.board_rider(visitor3)
+    visitor1.add_preference(:gentle)
+    visitor1.add_preference(:thrilling)
+    visitor2.add_preference(:gentle)
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:gentle)
+    visitor3.add_preference(:thrilling)
 
-      expect(carnival.most_popular_ride).to eq(ride1)
-    end
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+    carnival.add_ride(ride3)
+
+    ride3.board_rider(visitor3)
+    ride3.board_rider(visitor3)
+    ride2.board_rider(visitor1)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor3)
+  
+    expect(carnival.total_revenue).to eq(20)
+  end
 end
-
-
-# Additionally, use TDD to add the following functionality to the Carnival class. A passing challenge will complete at least one of the following. We recommend completing more than one if you have time.
-
-# A carnival can tell us its most popular ride.
-# A carnival can tell us its most profitable ride
-# A carnival can calculate the total revenue earned from all its rides.
